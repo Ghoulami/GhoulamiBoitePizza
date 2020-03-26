11aff73bd6c40dad97043a9c5fe01a49df46dfea
mehdi ghoulami <ghoulami97@gmail.com>
2020-03-26 16:16:51 +0100

    first commit

---
+++ routes/backpack/custom.php
@@ -0,0 +1,19 @@
+<?php
+
+// --------------------------
+// Custom Backpack Routes
+// --------------------------
+// This route file is loaded automatically by Backpack\Base.
+// Routes you generate using Backpack\Generators will be placed here.
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'prefix'     => config('backpack.base.route_prefix', 'admin'),
+    'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
+    'namespace'  => 'App\Http\Controllers\Admin',
+], function () { // custom admin routes
+    Route::crud('categorie', 'CategorieCrudController');
+    Route::crud('categorie', 'CategorieCrudController');
+    Route::crud('produit', 'ProduitCrudController');
+    Route::crud('produit', 'ProduitCrudController');
+}); // this should be the absolute last line of this file
